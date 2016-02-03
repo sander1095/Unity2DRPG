30b85e3e08b93ca2d70d91c8ab3a2a9da14b1e69
Sander ten Brinke <sander.ten.brinke@xs4all.nl>
2016-02-03 14:05:29 +0100

    Added Camera Control and fixed the ugly lines on the screen. (Video 5)

---
+++ ActionRPG/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject targetToFollow;
+    private Vector3 targetPosition;
+    public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        targetPosition = new Vector3(targetToFollow.transform.position.x, targetToFollow.transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+	}
+}

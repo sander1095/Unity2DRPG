3257c5ff8b187401ead7a9742a6459a5a49c1f23
Sander ten Brinke <sander.ten.brinke@xs4all.nl>
2016-02-04 11:05:26 +0100

    Made a Tile map (Video 6)

---
+++ ActionRPG/Assets/Art/Tiled Exported/base tileset.tsx
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="base tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1767">
+ <image source="../../../../../../../Desktop/roguelike-pack/Spritesheet/roguelikeSheet_transparent.png" width="968" height="526"/>
+ <terraintypes>
+  <terrain name="Dirt" tile="578"/>
+ </terraintypes>
+ <tile id="2">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index"/>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="60">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="116">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="-0.0909091" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="117">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="118">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="518" terrain="0,0,0,"/>
+ <tile id="519" terrain="0,0,,0"/>
+ <tile id="520" terrain=",,,0"/>
+ <tile id="521" terrain=",,0,0"/>
+ <tile id="522" terrain=",,0,"/>
+ <tile id="526">
+  <objectgroup draworder="index">
+   <object id="0" x="-0.363636" y="1.90909" width="16.3636" height="14.0909"/>
+  </objectgroup>
+ </tile>
+ <tile id="575" terrain="0,,0,0"/>
+ <tile id="576" terrain=",0,0,0"/>
+ <tile id="577" terrain=",0,,0"/>
+ <tile id="578" terrain="0,0,0,0"/>
+ <tile id="579" terrain="0,,0,"/>
+ <tile id="634" terrain=",0,,"/>
+ <tile id="635" terrain="0,0,,"/>
+ <tile id="636" terrain="0,,,"/>
+ <tile id="643">
+  <objectgroup draworder="index">
+   <object id="0" x="0" y="0.0625" width="15.9375" height="15.875"/>
+  </objectgroup>
+ </tile>
+</tileset>

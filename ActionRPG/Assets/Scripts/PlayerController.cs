7971c16777d10d12df2c1389c95ebeb31b85b9dd
Sander ten Brinke <sander.ten.brinke@xs4all.nl>
2016-02-05 15:18:17 +0100

    Completed video 8

--- ActionRPG/Assets/Scripts/PlayerController.cs
+++ ActionRPG/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 	public float moveSpeed;
 
 	private Animator anim;
+    private Rigidbody2D rb2d;
 
 	private bool playerMoving;
 	private Vector2 lastMove;
@@ -14,6 +15,7 @@ public class PlayerController : MonoBehaviour
 	void Start ()
 	{
 		anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -21,20 +23,27 @@ public class PlayerController : MonoBehaviour
 	{
 		playerMoving = false;
 		   
-		if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f )
-		{
-			transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f,0f));
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            rb2d.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, rb2d.velocity.y);
 			playerMoving = true;
 			lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
 		}
 
 		if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f )
 		{
-			transform.Translate(new Vector3(0f,Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime,0f));
+            rb2d.velocity = new Vector2(rb2d.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
 			playerMoving = true;
 			lastMove = new Vector2(0f,Input.GetAxisRaw("Vertical"));
 		}
 
+        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            rb2d.velocity = new Vector2(0f, rb2d.velocity.y);
+
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+            rb2d.velocity = new Vector2(rb2d.velocity.x, 0f);
+
+        //Set the animator stuff
 		anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
 		anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
 		anim.SetBool("PlayerMoving", playerMoving);
